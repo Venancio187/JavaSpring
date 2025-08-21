471460ef433263da18cba33411b04e18c4010018
Anna Venancio <annavenancio7@gmail.com>
2025-08-21 17:47:50 -0300

    Game repository, service, controller

---
+++ dslist/src/main/java/com/projeto/dslist/GameMinDTO.java
@@ -0,0 +1,45 @@
+package com.projeto.dslist;
+
+import com.projeto.dslist.entities.Game;
+
+public class GameMinDTO {
+	
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+
+   public GameMinDTO() {
+	   
+   }
+
+   public GameMinDTO(Game entity) {
+	this.id = entity.getId();
+	this.title = entity.getTitle();
+	this.year = entity.getYear();
+	this.imgUrl = entity.getImUrl();
+	this.shortDescription = entity.getShortDescription();
+   }
+
+   public Long getId() {
+	return id;
+   }
+
+   public String getTitle() {
+	return title;
+   }
+
+   public Integer getYear() {
+	return year;
+   }
+
+   public String getImgUrl() {
+	return imgUrl;
+   }
+
+   public String getShortDescription() {
+	return shortDescription;
+   }
+   
+}
